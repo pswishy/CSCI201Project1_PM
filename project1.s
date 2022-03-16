255b2722fb32ce5eb9b543a0ed248da06e634b2a
Pierce Medy <piercemedy24@gmail.com>
2022-03-16 11:56:36 -0400

    changed so that user interactively enters string

--- project1.s
+++ project1.s
@@ -1,5 +1,6 @@
 .data
-msg:  .asciiz "1111111111"
+userInput:  .space 11 # allow user to input string of 11 characters
+
 .text
 
 main: 
@@ -10,5 +11,16 @@ main:
       addi $t3, $0, 26 # constant 26 = t3
       add $t4, $t2, $t3 
       sub $t4, $t4, 1 # $t4 = 33 i.e max range for string values
+
+      li $v0, 8  # gets user input from keyboard
+      la $a0, userInput # stores user in put in $a0
+      li $a1, 11 # max lengeth of user input string
+      syscall
+
+      li $v0, 4
+      la $a0, userInput
+      syscall
+
       
-      
+      li $v0,10
+      syscall
