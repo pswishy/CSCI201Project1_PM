6bc78127b7a0dd40e4a1cbc9da0c0df128da35b5
Pierce Medy <piercemedy24@gmail.com>
2022-03-26 15:33:13 -0400

    creating sumnum label. incrementing byte address

--- project1.s
+++ project1.s
@@ -26,19 +26,22 @@ main:
       li $t1, 0 # iterator variable
       la $t9, userInput
 
-
+while:
       lb $s1, 0($t9) # lb load first char in $s1
       beq $t1, 10, exit
 
       blt $s1, 48, skipchar # 48 = '0' in ascii. if char < 48 skip it 
       ble $s1, 57, sumnum # 57 = '9' in ascii. if char <= 57 add it to sum
-
+      syscall
+      j while
 
 skipchar:
       
 sumnum:
       sub $s1, $s1, 48 # s1 = s1 - 48/ subtract 48 to get correct final sum val
-      add $t5, $t5, $s1
+      add $t0, $t0, $s1 # add val to $t5
+      sub $s1, $s1, $s1 # set $s1 back to 0
+      addi $t9, $t9, 1 # increment address for loop
 
 
 
