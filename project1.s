c490f885cee1bf4713b4660f802a2dbba926e204
Pierce Medy <piercemedy24@gmail.com>
2022-03-26 14:59:49 -0400

    realized did not need to store math in registers

--- project1.s
+++ project1.s
@@ -4,21 +4,26 @@ userInput:  .space 11 # allow user to input string of 11 characters
 .text
 
 main: 
-      addu $t0, $0, 0x2CBB81 # student id = $t0
-      addi $t1, $0, 11 # constant 11 = $t1
-      div $t0, $t1
-      mfhi $t2    # $t2 = 8 = 02931585 % 11
-      addi $t3, $0, 26 # constant 26 = t3
-      add $t4, $t2, $t3 
-      sub $t4, $t4, 1 # $t4 = 33 i.e max range for string values (only account for 23 letter in alphabet: W)
+      # addu $t0, $0, 0x2CBB81 # student id = $t0
+      # addi $t1, $0, 11 # constant 11 = $t1
+      # div $t0, $t1
+      # mfhi $t2    # $t2 = 8 = 02931585 % 11
+      # addi $t3, $0, 26 # constant 26 = t3
+      # add $t4, $t2, $t3 
+      # sub $t4, $t4, 1 # $t4 = 33 i.e max range for string values (only account for 23 letter in alphabet: W)
       # i only account for letters A-W
-      li $t5, 0 # sum variable stored in $t5
-      li $t6, 0 # iterator variable in $t6
+      # li $t5, 0 # sum variable stored in $t5
+      # li $t6, 0 # iterator variable in $t6
+
+      #-----------------------
       li $v0, 8  # gets user input from keyboard
       la $a0, userInput # stores user in put in $a0
       li $a1, 11 # max lengeth of user input string
       syscall
 
+      # -----------------------
+      li $t0, 0 # sum variable stored in $t0
+      li $t1, 0 # iterator variable
       la $t9, userInput
       
       lb $s1, 0($t9)
